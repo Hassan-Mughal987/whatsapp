130474f9af2b973603c04de38446399095db65bf
Hassan-Mughal987 <hassan@rapidevsol.com>
2025-04-15 17:59:58 +0500

    Home Changed

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:whatsapp/homescreen.dart';
 import 'package:whatsapp/splashscreen.dart';
-import '../../triptek/assets/intro.dart';
 import 'package:whatsapp/triptrek/splash1.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -15,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark(), // You can customize this theme
-      home: Intro(),
+      home: WhatsAppInterface(),
     );
   }
 }
