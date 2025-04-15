e8cfa98f795a8c7db9183deb783be86e4a935219
Hassan-Mughal987 <hassan@rapidevsol.com>
2025-04-15 18:16:15 +0500

    Removed Unsued imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:whatsapp/homescreen.dart';
 import 'package:whatsapp/splashscreen.dart';
-import 'package:whatsapp/triptrek/splash1.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
